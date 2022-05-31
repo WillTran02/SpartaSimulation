b74cc5220bb668836f3cf6bcf195250f4807c5d9
leehalperin <leehalperin@gmail.com>
2022-05-31 14:16:46 +0100

    git commit -m leescommit
    :wq

--- SpartaProject/src/main/java/com/sparta/getMonths/App.java
+++ SpartaProject/src/main/java/com/sparta/getMonths/App.java
@@ -1,5 +1,37 @@
 package com.sparta.getMonths;
 
-public class App {
+package com.sparta.lh;
 
+import java.util.Scanner;
+
+
+
+public class getMonths {
+
+    public static void main(String args[]) {
+
+        Scanner console = new Scanner(System.in);
+        String input;
+        int months;
+        boolean isValid;
+
+        do {
+            isValid = true; // reset the validity
+            System.out.println("Please enter the months of months you would like to run the simulation for: ");
+            input = console.nextLine();
+
+
+            try {
+                months = Integer.parseInt(input);
+                if (months < 12) {
+                    isValid = false;
+                    System.out.println("Please enter a minimum of 12 months: ");
+                }
+            } catch (NumberFormatException e) {
+                isValid = false;
+                System.out.println("You have not entered a months, please try again: ");
+            }
+
+        } while (!isValid);
+    }
 }
