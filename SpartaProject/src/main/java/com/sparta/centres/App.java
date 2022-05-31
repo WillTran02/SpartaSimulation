d0987852ce35fe4a48e96524d12bcd2e24d32421
farrah <farrahwaraich@hotmail.com>
2022-05-31 13:55:06 +0100

    Added centre methods

--- SpartaProject/src/main/java/com/sparta/centres/App.java
+++ SpartaProject/src/main/java/com/sparta/centres/App.java
@@ -1,5 +1,27 @@
 package com.sparta.centres;
 
-public class App {
+import java.util.Random;
 
+public class app {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int n = rand.nextInt(51);
+        System.out.println(n);
+
+        final Integer maxCapacity = new Integer(100);
+
+    }
+
+    public static boolean isFull (int capacity){
+        if(capacity >= 100){
+            return true;
+        } if (capacity < 100){
+        }
+        return false;
+    }
+
+    public int currentCapacity(int capacity){
+
+    }
 }
