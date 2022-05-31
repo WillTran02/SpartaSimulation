7ee6ed9a4f26cdea5acd8f4df5ac165aadbb6e42
vanisampath <vsampath@spartaglobal.com>
2022-05-31 14:25:05 +0100

    generatecenter

--- SpartaProject/src/main/java/com/sparta/generateCentres/App.java
+++ SpartaProject/src/main/java/com/sparta/generateCentres/App.java
@@ -1,5 +1,11 @@
 package com.sparta.generateCentres;
 
 public class App {
-
+    public  static int generatecenter(int months){
+        int center = 0;
+        if(months >= 2){
+            center = months/2;
+        }
+        return center;
+    }
 }
