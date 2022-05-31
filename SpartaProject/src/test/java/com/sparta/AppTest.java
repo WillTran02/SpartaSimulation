8d07e1df89ac66f3c8e6569385024aeea79a8347
Ahmed Yusuf <ahmedyusuf6195@gmail.com>
2022-05-31 13:56:43 +0100

    added test part to generateTrainees

--- SpartaProject/src/test/java/com/sparta/AppTest.java
+++ SpartaProject/src/test/java/com/sparta/AppTest.java
@@ -1,6 +1,21 @@
 package com.sparta;
 
 
+import com.sparta.generateTrainees.App;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
+    @Test
+    @DisplayName("check that it returns a number")
+    void checkNumberReturn() {
+
+        int var1 = App.generateTrainees(1);
+        int var2 = App.generateTrainees(1);
+
+        Assertions.assertEquals(false, var1 == var2);
+    }
+
 
 }
